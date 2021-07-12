a9cf94a83e0221a6e7a1e124a8376030a1d9d07c
Jamaria Sims <75871993+UnloadedZero@users.noreply.github.com>
2021-07-12 02:19:42 -0400

    ToDoList Done

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default Todos = () => {
+export default function Todos() {
+	const [inputValue, setInputValue] = useState("");
+	const [tdl, setTdl] = useState([]);
+
+	const changeInput = (evt) => {
+		const { value } = evt.target;
+		setInputValue(value);
+	};
+	useEffect(() => {
+		tdl.map((item) => {
+			if (item === inputValue) {
+				setInputValue("");
+				let holder = document.createElement("li");
+				holder.innerHTML = item;
+				holder.key = new Date();
+				holder.style.color = "red";
+				holder.addEventListener("click", () => {
+					holder.remove();
+				});
+				document.querySelector(".todos").appendChild(holder);
+			}
+		});
+	}, [tdl, setTdl]);
 	return (
 		<div className="widget-programmers container">
 			<h2>To Do List</h2>
-			<form>
-				<label>
-					Action
-					<input type="text" value="" onChange="" />
-				</label>
-				<input type="submit" value="Submit" />
-			</form>
-			<div className="list"></div>
+			<div>
+				<input
+					id="input-todolist"
+					type="text"
+					onChange={changeInput}
+					value={inputValue}
+				/>
+				<button
+					id="resetInput"
+					onClick={() =>
+						!tdl.includes(inputValue) && inputValue.length > 0
+							? setTdl((tdl) => [...tdl, inputValue])
+							: null
+					}
+				>
+					Add
+				</button>
+			</div>
+
+			<ol className="todos"></ol>
 		</div>
 	);
-};
+}
