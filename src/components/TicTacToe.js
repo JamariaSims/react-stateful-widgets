acf81e20e9a43ea18ea0d68f53029449930d6b3f
Jamaria Sims <75871993+UnloadedZero@users.noreply.github.com>
2021-07-12 07:19:04 -0400

    Tic-Tac-Toe Part-1

---
+++ src/components/TicTacToe.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class TicTacToe extends Component {
+	render() {
+		return <div className="widget-TicTacToe container"></div>;
+	}
+}
+
+export default TicTacToe;
